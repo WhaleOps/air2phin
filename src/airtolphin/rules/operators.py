d8bd32ed01dd585bf81fa8299b444c01f5e33e6f
Jiajie Zhong <zhongjiajie955@hotmail.com>
2021-12-01 17:41:25 +0800

    Add dummy as new convert operator
    fix: #6

--- src/airtolphin/rules/operators.py
+++ src/airtolphin/rules/operators.py
@@ -2,6 +2,8 @@ import libcst as cst
 
 
 class Operators:
+    """Covert Airflow Operator to DolphinScheduler Task."""
+
     attr_keep_old = (
         "whitespace_before_args",
     )
@@ -14,17 +16,38 @@ class Operators:
         call_kwargs = {}
         call_args = []
         call = cst.ensure_type(self.node.value, cst.Call)
-        # covert `airflow.operators.bash.BashOperator` assign
-        if cst.ensure_type(call.func, cst.Name).value == "BashOperator":
-            call_kwargs["func"] = cst.Name("Shell")
-            for arg in call.args:
-                arg_keyword = cst.ensure_type(arg.keyword, cst.Name).value
-                if arg_keyword == "task_id":
-                    call_args.append(arg.with_changes(keyword=cst.Name("name")))
-                elif arg_keyword == "bash_command":
-                    call_args.append(arg.with_changes(keyword=cst.Name("command")))
-            call_kwargs["args"] = call_args
-            for attr in self.attr_keep_old:
-                call_kwargs[attr] = getattr(call, attr)
-            return self.node.with_changes(value=cst.Call(**call_kwargs))
+        # TODO: UT on it ex: `common_path = Variable.get("common_path")`
+        if isinstance(call.func, cst.Name):
+            # covert `airflow.operators.bash.BashOperator` assign
+            if cst.ensure_type(call.func, cst.Name).value == "BashOperator":
+                call_kwargs["func"] = cst.Name("Shell")
+                for arg in call.args:
+                    arg_keyword = cst.ensure_type(arg.keyword, cst.Name).value
+                    if arg_keyword == "task_id":
+                        call_args.append(arg.with_changes(keyword=cst.Name("name")))
+                    elif arg_keyword == "bash_command":
+                        call_args.append(arg.with_changes(keyword=cst.Name("command")))
+                call_kwargs["args"] = call_args
+                for attr in self.attr_keep_old:
+                    call_kwargs[attr] = getattr(call, attr)
+                return self.node.with_changes(value=cst.Call(**call_kwargs))
+            # covert `airflow.operators.dummy` assign, dolphinscheduler without dummy operator so we use bash
+            # task with `echo 'airflow dummy operator'`
+            if cst.ensure_type(call.func, cst.Name).value == "DummyOperator":
+                call_kwargs["func"] = cst.Name("Shell")
+                for arg in call.args:
+                    arg_keyword = cst.ensure_type(arg.keyword, cst.Name).value
+                    if arg_keyword == "task_id":
+                        call_args.append(arg.with_changes(keyword=cst.Name("name")))
+
+                        call_args.append(
+                            cst.Arg(
+                                keyword=cst.Name("command"),
+                                value=cst.SimpleString("\"echo 'airflow dummy operator'\""),
+                            )
+                        )
+                call_kwargs["args"] = call_args
+                for attr in self.attr_keep_old:
+                    call_kwargs[attr] = getattr(call, attr)
+                return self.node.with_changes(value=cst.Call(**call_kwargs))
         return self.node
