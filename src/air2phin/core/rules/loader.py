44517e947d0e839db55e31d7b4312672fbb1a990
Jay Chung <zhongjiajie955@gmail.com>
2023-03-14 11:07:05 +0800

    feat: New rule trigger rule

--- src/air2phin/core/rules/loader.py
+++ src/air2phin/core/rules/loader.py
@@ -7,9 +7,10 @@ path_rule = project.joinpath("rules")
 path_operators = path_rule.joinpath("operators")
 path_hooks = path_rule.joinpath("hooks")
 path_models = path_rule.joinpath("models")
+path_utils = path_rule.joinpath("utils")
 path_dag_cnx = path_rule.joinpath("core", "dagContext.yaml")
 
-rule_imports = [path_dag_cnx, path_operators, path_hooks, path_models]
+rule_imports = [path_dag_cnx, path_operators, path_hooks, path_models, path_utils]
 
 rule_calls = [path_dag_cnx, path_operators, path_hooks, path_models]
 
