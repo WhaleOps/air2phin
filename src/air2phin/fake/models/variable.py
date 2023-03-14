e7176026536f16b014d3b08900b349b94776dd6a
Jay Chung <zhongjiajie955@gmail.com>
2023-03-14 14:49:22 +0800

    fix: Add optional param for Variable setting

--- src/air2phin/fake/models/variable.py
+++ src/air2phin/fake/models/variable.py
@@ -5,18 +5,41 @@ class Variable:
     """Mock airflow.models.Variable class, make migrator do less thing."""
 
     @classmethod
-    def get(cls, key: str) -> str:
+    def get(
+        cls,
+        key: str,
+        default_var: Any,
+        *args,
+        **kwargs,
+    ) -> str:
         """Modck airflow.models.Variable.get method, only return income parameter."""
-        return key
+        return key if default_var is None else default_var
 
     @classmethod
-    def set(cls, key: str, value: Any) -> str:
+    def set(
+        cls,
+        key: str,
+        value: Any,
+        *args,
+        **kwargs,
+    ) -> str:
         """Mock airflow.models.Variable.set method, do nothing."""
 
     @classmethod
-    def update(cls, key: str, value: Any) -> str:
+    def update(
+        cls,
+        key: str,
+        value: Any,
+        *args,
+        **kwargs,
+    ) -> str:
         """Mock airflow.models.Variable.update method, do nothing."""
 
     @classmethod
-    def delete(cls, key: str) -> str:
+    def delete(
+        cls,
+        key: str,
+        *args,
+        **kwargs,
+    ) -> str:
         """Mock airflow.models.Variable.delete method, do nothing."""
